f675d4fd961d57018f0f3a66ac7c03d494c36571
mocherlaswetha <112679777+mocherlaswetha@users.noreply.github.com>
2022-11-04 12:27:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = express.Router();
+
+router.get("/",function (req, res)  {
+  res.sendFile(path.join('C:/Users/Admin/index.html'));
+});
+app.use("/", router);
+app.listen(process.env.port || 3000);
+
+console.log("Running at Port 3000");
